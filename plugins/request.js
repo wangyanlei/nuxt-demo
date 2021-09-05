7f0a895cb66a351dd7c2afd775a1189320888621
wyl <protolei@gmail.com>
2021-09-05 15:57:55 +0800

    修改baseURL

--- plugins/request.js
+++ plugins/request.js
@@ -5,8 +5,8 @@
  import axios from 'axios'
 
  export const request = axios.create({
-  //  baseURL: 'https://conduit.productionready.io'
-   baseURL: 'http://realworld.api.fed.lagounews.com'
+   baseURL: 'https://conduit.productionready.io'
+  //  baseURL: 'http://realworld.api.fed.lagounews.com'
  })
  
 //  通过插件机制获取到上下文对象
